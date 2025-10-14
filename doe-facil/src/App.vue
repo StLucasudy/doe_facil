d1a3ff23c9bce0342387cc93e82b5189c87a9432
StLucasdy <lucas.2502486@aluno.faculdadeimpacta.com.br>
2025-10-14 09:59:48 -0300

    14_10_2025:iniciando desenvolvimento do front

---
+++ doe-facil/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+      <LoginPage />
+  </div>
+</template>
+
+<script setup>
+import LoginPage from './components/LoginPage.vue';
+</script>
+
+
+<style>
+  container {
+    align-items: center;
+  }
+</style>
---
+++ doe-facil/src/components/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+        <img src="./assets/doe_facil.svg">
+        <h1>{{ nomeApp }}</h1>
+        <button>Login</button>
+        <button>Cadastro</button>
+</template>
+
+<script setup>
+    const nomeApp = 'Doe fácil';
+</script>
+
+<style scoped>
+
+    h1 {
+     color: rgb(255, 255, 255);
+    }
+</style>
